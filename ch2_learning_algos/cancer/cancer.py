50efb15b66c50a44ad72a2762243c0b1dd8c3a8e
@Pavlo.Bohdan <pavlo.bohdan@nordstrom.com>
2020-01-26 20:22:13 +0200

    Add comment in cancer.py

--- ch2_learning_algos/cancer/cancer.py
+++ ch2_learning_algos/cancer/cancer.py
@@ -3,6 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
+# Measure training and test accuracy
+# for breast_cancer dataset
+# using K-neighbor classifier
+# with K from 1 through 10
 cancer = load_breast_cancer()
 
 X_train, X_test, y_train, y_test = train_test_split(
