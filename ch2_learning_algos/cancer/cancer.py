0c94c99486f7cc3d8c9a39303ebb38b89c42423d
@Pavlo.Bohdan <pavlo.bohdan@nordstrom.com>
2020-01-26 19:08:30 +0200

    Begin working on cancer.py

--- ch2_learning_algos/cancer/cancer.py
+++ ch2_learning_algos/cancer/cancer.py
@@ -1,7 +1,4 @@
 from sklearn.datasets import load_breast_cancer
 import numpy as np
 
-d = {'adfgfdswdfg': 1}
 cancer = load_breast_cancer()
-print(dict(zip(cancer.target_names, np.bincount(cancer.target))))
-print(cancer.feature_names)
